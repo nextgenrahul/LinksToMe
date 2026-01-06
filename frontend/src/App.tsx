f931dbabce0fb30edb3a7df12c8b5e7e5b579c34
Rahul Shakya <rahulshakya7685@gmail.com>
2026-01-06 20:38:47 +0530

    refactor: Remove ThemeProvider and related theme context files, simplify App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
-import { ThemeProvider } from "./context/ThemeContext";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 function App() {
   return (
-    <ThemeProvider>
       <RouterProvider router={router} />
-    </ThemeProvider>
   );
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from '@/store';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
